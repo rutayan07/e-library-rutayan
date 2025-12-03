8d14056422345944808483bd949b95c042ecfc23
Rutayan <patrarutayan@gmail.com>
2025-12-03 09:31:20 +0530

    Obscure database password in DBConnection.java
    Replaced the database password with a placeholder for security.

--- backend/src/main/java/com/elibrary/dao/DBConnection.java
+++ backend/src/main/java/com/elibrary/dao/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/elibrary?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASS = "RutayanOP07";
+    private static final String PASS = "*********";
 
     static {
         try { Class.forName("com.mysql.cj.jdbc.Driver"); } catch (ClassNotFoundException e) { e.printStackTrace(); }
